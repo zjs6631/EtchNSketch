0e05802e06245dbc52691337053c714237592cd9
zjs6631 <zacharyjonessmith@gmail.com>
2022-03-31 21:57:17 -0400

    completed the etch n sketch. Made container height n width update properly.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,22 @@
 let container = document.getElementById("Container");
-container.style.backgroundColor = "green";
+container.style.backgroundColor = "black";
 
-let l = 16;
-let w = 16;
+let l = prompt("How many squares high would you like the grid?");
+let w = prompt("How many square wide would you like the grid?");
+console.log(container.style.height)
+console.log(container.style.width)
+
+
+
 for (i = 0; i < (l * w); i++){
+    
+   
+    containerHeight = (l * 12);
+    containerWidth = (w * 12);
+    container.style.height = containerHeight + "px";
+    container.style.width = containerWidth + "px";
+    container.style.maxHeight = containerHeight + "px";
+    container.style.maxWidth = containerWidth + "px";
     let div = document.createElement('div');
     div.classList.add('grid');
     div.style.backgroundColor = 'aqua';
@@ -13,16 +26,19 @@ for (i = 0; i < (l * w); i++){
     div.style.height = "10px";
     
     div.addEventListener('mouseover', ()=>{
-        div.style.backgroundColor = "purple";
+        div.style.backgroundColor = "black";
+    })
+
+    const clearBtn = document.getElementById("clear");
+
+    clearBtn.addEventListener('click', ()=>{
+        div.style.backgroundColor = "aqua";
+        
     })
     container.appendChild(div); 
 }
 
-const clearBtn = document.getElementById("clear");
 
-clearBtn.addEventListener('click', ()=>{
-    
-})
 
 
 
